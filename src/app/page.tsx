b4b7d811a3c8be5f7ad0b79534523d99d6e87872
Reinhard Dave Yunardi <126077909+reinharddaveyunardi@users.noreply.github.com>
2024-09-23 00:33:29 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import Hero from "./layout/Hero";
-import { Skills } from "./layout/Skills";
-
 export default function Home() {
     return (
         <div>
             <Hero />
-            {/* <Skills /> */}
         </div>
     );
 }
